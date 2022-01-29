a84fd137f0e4e8dac0e1ffa60ea39788abc5204f
nakata <54203719+heyhey1007@users.noreply.github.com>
2022-01-30 01:01:39 +0900

    #30 一覧画面基本的なレイアウトの実装

---
+++ components/SearchListItem.vue
@@ -0,0 +1,64 @@
+<template>
+  <div :href="link" class="search-list-item-container pb-2">
+    <v-row align="start">
+      <v-col cols="12">
+        <div class="hairdresser-name pl-2">{{ hairdresser.name }}</div>
+      </v-col>
+    </v-row>
+    <v-row align-content="center" justify="center">
+      <v-col cols="12" class="hairdresser-swiper py-0">
+        <client-only>
+          <swiper class="swiper" :options="swiperOption">
+            <swiper-slide
+              v-for="(item, i) in hairdresser.images"
+              :key="i"
+              reverse-transition="fade-transition"
+              transition="fade-transition"
+            >
+              <v-img
+                :src="item.src"
+                :lazy-src="item.src"
+                class="grey lighten-2"
+              />
+            </swiper-slide>
+          </swiper>
+        </client-only>
+      </v-col>
+    </v-row>
+    <v-row align="start">
+      <v-col cols="12" class="ml-2">
+        <p class="hairdresser-summary">{{ hairdresser.summary }}</p>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    hairdresser: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      link: `#${this.$props.hairdresser.id}`,
+      swiperOption: {
+        slidesPerView: 3,
+        spaceBetween: 8,
+      },
+    }
+  },
+}
+</script>
+
+<style>
+.hairdresser-name {
+  font: bold;
+  font-size: 1.4rem;
+}
+.v-img {
+  height: 100%;
+}
+</style>
---
+++ pages/searchList.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-app>
+    <v-main class="my-6 pt-0">
+      <v-container fluid class="search-form">
+        <SearchSection />
+      </v-container>
+      <v-container fluid>
+        <div id="search-result">
+          <li v-for="item in result" :key="item.id">
+            <v-divider></v-divider>
+            <SearchListItem class="my-2" :hairdresser="item" />
+          </li>
+        </div>
+        <v-divider></v-divider>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    result: [
+      {
+        id: 1,
+        name: 'フリーランス太郎',
+        summary: 'テストです。',
+        images: [
+          { src: require('~/assets/img/1.svg'), alt: 'image1' },
+          { src: require('~/assets/img/2.svg'), alt: 'image1' },
+          { src: require('~/assets/img/1.svg'), alt: 'image1' },
+          { src: require('~/assets/img/2.svg'), alt: 'image1' },
+        ],
+        discription:
+          'kakak\nkakaka\nkakaka\nkakaka\nkakaka\nkakaka\nkakaka\nkakaka\nkakaka\nkakaka\nkakaka\nkakaka',
+      },
+      {
+        id: 2,
+        name: 'フリーランス花子',
+        summary: 'テストですーーーーー',
+        images: [
+          { src: require('~/assets/img/1.svg'), alt: 'image1' },
+          { src: require('~/assets/img/2.svg'), alt: 'image1' },
+          { src: require('~/assets/img/1.svg'), alt: 'image1' },
+          { src: require('~/assets/img/2.svg'), alt: 'image1' },
+        ],
+        discription:
+          'kakak\nkakaka\nkakaka\nkakaka\nkakaka\nkakaka\nkakaka\nkakaka\nkakaka\nkakaka\nkakaka\nkakaka',
+      },
+      {
+        id: 3,
+        name: 'フリーランス大阪',
+        summary: 'テストですーーーー!!!!',
+        images: [
+          { src: require('~/assets/img/1.svg'), alt: 'image1' },
+          { src: require('~/assets/img/1.svg'), alt: 'image1' },
+          { src: require('~/assets/img/1.svg'), alt: 'image1' },
+          { src: require('~/assets/img/2.svg'), alt: 'image1' },
+        ],
+        discription:
+          'kakak\nkakaka\nkakaka\nkakaka\nkakaka\nkakaka\nkakaka\nkakaka\nkakaka\nkakaka\nkakaka\nkakaka',
+      },
+    ],
+    flat: null,
+  }),
+}
+</script>
+
+<style lang="scss" scoped>
+li {
+  list-style-type: none;
+}
+#search-result {
+  padding: 0 10 0 10;
+}
+</style>
