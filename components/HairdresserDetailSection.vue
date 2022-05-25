7b8ba04bef6aa8163df8ae6bd9dbbe7d497b447e
fumihub <60814954+fumihub@users.noreply.github.com>
2022-05-25 23:20:12 +0900

    #37 エラーハンドリング修正

--- components/HairdresserDetailSection.vue
+++ components/HairdresserDetailSection.vue
@@ -1,129 +1,127 @@
 <template>
-    <section id="carouselPicture">
-      <v-container fluid>
-          <v-row align="center" justify="center">
-            <v-col cols="12" sm="10" md="9" lg="7" xl="6">
-              <h1 align="center" justify="center" class="font-weight-light display-2">
-                  {{ lastName + " " + firstName }}
-              </h1>
-              <h1 class="font-weight-bold my-3">
-                詳細情報
-              </h1>
-                <v-data-table
-                  :headers="headers"
-                  :items="informations"
-                  hide-default-header
-                  hide-default-footer
-                  class="elevation-1"
-                ></v-data-table>
-            </v-col>
-          </v-row>
-      </v-container>
-    </section>
+  <section id="carouselPicture">
+    <v-container fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="10" md="9" lg="7" xl="6">
+          <h1
+            align="center"
+            justify="center"
+            class="font-weight-light display-2"
+          >
+            {{ lastName + ' ' + firstName }}
+          </h1>
+          <h1 class="font-weight-bold my-3">詳細情報</h1>
+          <v-data-table
+            :headers="headers"
+            :items="informations"
+            hide-default-header
+            hide-default-footer
+            class="elevation-1"
+          ></v-data-table>
+        </v-col>
+      </v-row>
+    </v-container>
+  </section>
 </template>
 
 <script>
-  
-  export default {
-    props:{
-      lastName: {
-        type: String,
-        required: true
-      },
-      firstName: {
-        type: String,
-        required: true
-      },
-      phoneNumber: {
-        type: String,
-        required: true
-      },
-      area: {
-        type: String,
-        required: true
-      },
-      startTime: {
-        type: Array,
-        required: true
-      },
-      endTime: {
-        type: String,
-        required: true
-      },
-      payments: {
-        type: Array,
-        required: true
-      },
-      services: {
-        type: Array,
-        required: true
-      },
-      goodMenus: {
-        type: Array,
-        required: true
-      },
-      holidays: {
-        type: Array,
-        required: true
-      },
+export default {
+  props: {
+    lastName: {
+      type: String,
+      required: true,
     },
-    data () {
-      return {
-        headers: [
-          { text: '項目', value: 'category' },
-          { text: '情報', value: 'information' },
-        ],
-        informations: [],
-      }
+    firstName: {
+      type: String,
+      required: true,
     },
-    created() {
-      // 電話番号代入
-      const phoneNumber = {
-        category: "電話番号",
-        information: this.phoneNumber,
-      };
-      this.informations.push(phoneNumber);
-      // エリア代入
-      const area = {
-        category: "対応可能エリア",
-        information: this.area,
-      };
-      this.informations.push(area);
-      // 営業時間代入
-      const time = {
-        category: "営業時間",
-        information: `${this.startTime} ~ ${this.endTime}`,
-      };
-      this.informations.push(time);
-      // 支払い方法代入
-      const payment = {
-        category: "支払い方法",
-        information: this.payments.join(' / '),
-      };
-      this.informations.push(payment);
-      // 設備・サービス代入
-      const service = {
-        category: "設備・サービス",
-        information: this.services.join(' / '),
-      };
-      this.informations.push(service);
-      // 得意メニュー代入
-      const goodMenu = {
-        category: "得意メニュー",
-        information: this.goodMenus.join(' / '),
-      };
-      this.informations.push(goodMenu);
-      // 定休日代入
-      const holiday = {
-        category: "得意メニュー",
-        information: this.holidays.join(' / '),
-      };
-      this.informations.push(holiday);
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    area: {
+      type: String,
+      required: true,
+    },
+    startTime: {
+      type: Array,
+      required: true,
+    },
+    endTime: {
+      type: String,
+      required: true,
+    },
+    payments: {
+      type: Array,
+      required: true,
+    },
+    services: {
+      type: Array,
+      required: true,
+    },
+    goodMenus: {
+      type: Array,
+      required: true,
+    },
+    holidays: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      headers: [
+        { text: '項目', value: 'category' },
+        { text: '情報', value: 'information' },
+      ],
+      informations: [],
     }
-  }
+  },
+  created() {
+    // 電話番号代入
+    const phoneNumber = {
+      category: '電話番号',
+      information: this.phoneNumber,
+    }
+    this.informations.push(phoneNumber)
+    // エリア代入
+    const area = {
+      category: '対応可能エリア',
+      information: this.area,
+    }
+    this.informations.push(area)
+    // 営業時間代入
+    const time = {
+      category: '営業時間',
+      information: `${this.startTime} ~ ${this.endTime}`,
+    }
+    this.informations.push(time)
+    // 支払い方法代入
+    const payment = {
+      category: '支払い方法',
+      information: this.payments.join(' / '),
+    }
+    this.informations.push(payment)
+    // 設備・サービス代入
+    const service = {
+      category: '設備・サービス',
+      information: this.services.join(' / '),
+    }
+    this.informations.push(service)
+    // 得意メニュー代入
+    const goodMenu = {
+      category: '得意メニュー',
+      information: this.goodMenus.join(' / '),
+    }
+    this.informations.push(goodMenu)
+    // 定休日代入
+    const holiday = {
+      category: '定休日',
+      information: this.holidays.join(' / '),
+    }
+    this.informations.push(holiday)
+  },
+}
 </script>
 
-<style lang="scss" scoped>
-
-
-</style>
+<style lang="scss" scoped></style>
--- pages/inputBeauticianInformation.vue
+++ pages/inputBeauticianInformation.vue
@@ -1,39 +1,26 @@
 <template>
   <v-app>
     <v-main class="pt-0">
-      <v-tabs
-        v-model="tab"
-        grow
-      >
+      <v-tabs v-model="tab" grow>
         <v-tabs-slider color="yellow"></v-tabs-slider>
-        <v-tab
-          v-for="topMenu in topMenus"
-          :key="topMenu"
-        >
+        <v-tab v-for="topMenu in topMenus" :key="topMenu">
           {{ topMenu }}
         </v-tab>
       </v-tabs>
       <v-tabs-items v-model="tab">
-        <v-tab-item
-          v-for="topMenu in topMenus"
-          :key="topMenu"
-        >
+        <v-tab-item v-for="topMenu in topMenus" :key="topMenu">
           <!-- 詳細情報入力画面 -->
           <div v-if="topMenu === '詳細情報'">
-            <beautician-profile-input 
-              @transmitProfile = "inputProfile"
-            />
+            <beautician-profile-input @transmitProfile="inputProfile" />
           </div>
           <!-- カットメニュー入力画面 -->
           <div v-else-if="topMenu === 'カットメニュー'">
-            <beautician-menu-input 
-              @transmitMenu = "inputMenu"
-            />
+            <beautician-menu-input @transmitMenu="inputMenu" />
           </div>
           <!-- プロフィール画像入力画面 -->
           <div v-else-if="topMenu === 'プロフィール画像'">
-            <beautician-profile-picture-input 
-              @transmitProfilePicture = "inputProfilePicture"
+            <beautician-profile-picture-input
+              @transmitProfilePicture="inputProfilePicture"
             />
           </div>
         </v-tab-item>
@@ -68,17 +55,19 @@ import BeauticianProfilePictureInput from '../components/BeauticianProfilePictur
 import BeauticianProfileInput from '../components/BeauticianProfileInput.vue'
 export default {
   name: 'InputBeauticianInformation',
-  components: { BeauticianProfileInput, BeauticianMenuInput, BeauticianProfilePictureInput },
+  components: {
+    BeauticianProfileInput,
+    BeauticianMenuInput,
+    BeauticianProfilePictureInput,
+  },
   data() {
     return {
       fab: null,
       color: '',
       flat: null,
       tab: null,
-      topMenus: [
-        '詳細情報', 'カットメニュー', 'プロフィール画像',
-      ],
-      profile:{},
+      topMenus: ['詳細情報', 'カットメニュー', 'プロフィール画像'],
+      profile: {},
       menus: [],
       profilePictures: [],
     }
@@ -116,204 +105,214 @@ export default {
     },
     inputProfile(profile) {
       // inputProfileをクリア
-      this.profile = {};
+      this.profile = {}
       // BeauticianProfileInputから帰ってきた値を保持
-      this.profile = Object.assign({}, profile);
-    }
-    ,
+      this.profile = Object.assign({}, profile)
+    },
     inputMenu(menus) {
       // menuをクリア
-      this.menus = [];
+      this.menus = []
       // BeauticianMenuInputから帰ってきた値を保持
-      this.menus = menus;
-    }
-    ,
+      this.menus = menus
+    },
     inputProfilePicture(profilePictures) {
       // profilePictureをクリア
-      this.profilePictures = [];
+      this.profilePictures = []
       // BeauticianProfilePictureInputから帰ってきた値を保持
-      this.profilePictures = profilePictures;
+      this.profilePictures = profilePictures
     },
     transition() {
-      let error = false;
+      const profileErrorMessage = []
+      const menuErrorMessage = []
+      const profilePictureErrorMessage = []
+      let error = false
       // エラー処理(profile)----------
       // profileが空の場合
       if (this.isEmpty(this.profile)) {
-        alert("詳細情報を入力してください。");
-        error = true;
+        profileErrorMessage.push('プロフィールを入力してください。')
       }
       // profileが空でない場合
       else {
-        // 定休日にその他の入力がないか確認
-        let holidayCheck = false;
-        for (let i = 0; i < this.profile.holidaySelect.length; i++) {
-          if (this.profile.holidaySelect[i] === "月"
-          || this.profile.holidaySelect[i] === "火"
-          || this.profile.holidaySelect[i] === "水"
-          || this.profile.holidaySelect[i] === "木"
-          || this.profile.holidaySelect[i] === "金"
-          || this.profile.holidaySelect[i] === "土"
-          || this.profile.holidaySelect[i] === "日") {
-            holidayCheck = true;
-          }
-          else {
-            holidayCheck = false;
-            break;
-          }
+        // 名前が未入力の場合
+        if (
+          this.profile.lastName === null ||
+          this.profile.firstName === null ||
+          this.profile.lastName === '' ||
+          this.profile.firstName === ''
+        ) {
+          profileErrorMessage.push('名前を入力してください。')
         }
-        // 名前が未入力
-        if (this.profile.lastName === null
-        || this.profile.firstName === null) {
-          alert("名前を入力してください。");
-          error = true;
-        }
-        // 電話番号が未入力
-        else if (this.profile.phoneNumber === null) {
-          alert("電話番号を入力してください。");
-          error = true;
+        // 電話番号が未入力の場合
+        if (
+          this.profile.phoneNumber === null ||
+          this.profile.phoneNumber === ''
+        ) {
+          profileErrorMessage.push('電話番号を入力してください。')
         }
         // エリアが未入力
-        else if (this.profile.area === null) {
-          alert("電話番号を入力してください。");
-          error = true;
+        if (this.profile.area === null || this.profile.area === '') {
+          profileErrorMessage.push('エリアを入力してください。')
         }
         // 営業時間が未入力
-        else if (this.profile.startTime === null
-        || this.profile.endTime === null) {
-          alert("営業時間を入力してください。");
-          error = true;
+        if (this.profile.startTime === null || this.profile.endTime === null) {
+          profileErrorMessage.push('営業時間を入力してください。')
         }
         // 営業時間の開始時間が終了時間より遅い、または終了時間が開始時間より早い
-        else if (this.profile.startTime > this.profile.endTime) {
-          alert("営業時間が正確ではありません。正確な営業時間を入力してください。");
-          error = true;
+        if (this.profile.startTime > this.profile.endTime) {
+          profileErrorMessage.push(
+            '営業時間が正確ではありません。正確な営業時間を入力してください。'
+          )
         }
         // 支払い方法が未入力
-        else if (this.profile.paymentSelect.length === 0) {
-          alert("支払い方法を入力してください。");
-          error = true;
+        if (this.profile.paymentSelect.length === 0) {
+          profileErrorMessage.push('支払い方法を入力してください。')
         }
         // 設備・サービスが未入力
-        else if (this.profile.serviceSelect.length === 0) {
-          alert("設備・サービスを入力してください。");
-          error = true;
+        if (this.profile.serviceSelect.length === 0) {
+          profileErrorMessage.push('設備・サービスを入力してください。')
         }
         // 得意メニューが未入力
-        else if (this.profile.goodMenuSelect.length === 0) {
-          alert("得意メニューを入力してください。");
-          error = true;
+        if (this.profile.goodMenuSelect.length === 0) {
+          profileErrorMessage.push('得意メニューを入力してください。')
         }
         // 定休日が未入力
-        else if (this.profile.holidaySelect.length === 0) {
-          alert("定休日を入力してください。");
-          error = true;
+        if (this.profile.holidaySelect.length === 0) {
+          profileErrorMessage.push('定休日を入力してください。')
         }
-        // 定休日に任意の文字が入力されている
-        else if (!holidayCheck) {
-          alert("定休日は選択肢から選んでください。");
-          error = true;
+        // 定休日が入力されている場合
+        else {
+          // 定休日にその他の入力がないか確認
+          for (let i = 0; i < this.profile.holidaySelect.length; i++) {
+            // 定休日に任意の文字が入力されている
+            if (
+              this.profile.holidaySelect[i] !== '月' &&
+              this.profile.holidaySelect[i] !== '火' &&
+              this.profile.holidaySelect[i] !== '水' &&
+              this.profile.holidaySelect[i] !== '木' &&
+              this.profile.holidaySelect[i] !== '金' &&
+              this.profile.holidaySelect[i] !== '土' &&
+              this.profile.holidaySelect[i] !== '日' &&
+              this.profile.holidaySelect[i] !== '祝日' &&
+              this.profile.holidaySelect[i] !== 'なし'
+            ) {
+              profileErrorMessage.push('定休日は選択肢から選んでください。')
+              break
+            }
+          }
         }
+        // 自己紹介が未入力の場合
+        if (
+          this.profile.introduction === null ||
+          this.profile.introduction === ''
+        ) {
+          profileErrorMessage.push('自己紹介を入力してください。')
+        }
+      }
+      // エラー出力
+      if (profileErrorMessage.length) {
+        alert(profileErrorMessage.join('\n'))
+        error = true
       }
       // ---------------------------
       // エラー処理(menus)-------------
-      // menuが空の場合
+      // menusが空の場合
       if (this.isEmpty(this.menus)) {
-        alert("カットメニューを入力してください。")
-        error = true;
+        menuErrorMessage.push('カットメニューを入力してください。')
       }
-      // menuが空でない場合
+      // menusが空でない場合
       else {
-        // 処置時間にその他の入力がないか確認
-        let timeCheck = false;
-        for (let i = 0; i < this.menus.length; i++) {
-          if (this.menus[i].timeSelect === "15分"
-          || this.menus[i].timeSelect === "30分"
-          || this.menus[i].timeSelect === "1時間"
-          || this.menus[i].timeSelect === "1時間30分"
-          || this.menus[i].timeSelect === "2時間"
-          || this.menus[i].timeSelect === "2時間30分"
-          || this.menus[i].timeSelect === "3時間"
-          || this.menus[i].timeSelect === "3時間30分"
-          || this.menus[i].timeSelect === "4時間"
-          || this.menus[i].timeSelect === "4時間30分"
-          || this.menus[i].timeSelect === "5時間"
-          || this.menus[i].timeSelect === null) {
-            timeCheck = true;
-          }
-          else {
-            timeCheck = false;
-            break;
-          }
-        }
-        for (let i = 0; i < this.menus.length; i++) {
+        this.menus.forEach((menu, index) => {
           // カットメニュー名が未入力
-          if (this.menus[i].menu === null
-          || this.menus[i].menu === "") {
-            alert("カットメニュー名を入力してください。");
-            error = true;
-            break;
+          if (menu.menu === null || menu.menu === '') {
+            menuErrorMessage.push(
+              `メニュー${index + 1}のカットメニュー名を入力してください。`
+            )
           }
           // メニュー詳細が未入力
-          else if (this.menus[i].menuDetail === null
-          || this.menus[i].menuDetail === "") {
-            alert("メニュー詳細を入力してください。");
-            error = true;
-            break;
+          if (menu.menuDetail === null || menu.menuDetail === '') {
+            menuErrorMessage.push(
+              `メニュー${index + 1}のメニュー詳細を入力してください。`
+            )
           }
           // 料金が未入力
-          else if (this.menus[i].price === null
-          || this.menus[i].price === "") {
-            alert("料金を入力してください。");
-            error = true;
-            break;
+          if (menu.price === null || menu.price === '') {
+            menuErrorMessage.push(
+              `メニュー${index + 1}の料金を入力してください。`
+            )
           }
           // 処置時間が未入力
-          else if (this.menus[i].timeSelect === null
-          || this.menus[i].timeSelect === "") {
-            alert("処置時間を入力してください。");
-            error = true;
-            break;
+          if (menu.timeSelect === null || menu.timeSelect === '') {
+            menuErrorMessage.push(
+              `メニュー${index + 1}の処置時間を入力してください。`
+            )
+          } else {
+            // 処置時間に任意の文字が入力されている
+            // eslint-disable-next-line no-lonely-if
+            if (
+              menu.timeSelect !== '15分' &&
+              menu.timeSelect !== '30分' &&
+              menu.timeSelect !== '1時間' &&
+              menu.timeSelect !== '1時間30分' &&
+              menu.timeSelect !== '2時間' &&
+              menu.timeSelect !== '2時間30分' &&
+              menu.timeSelect !== '3時間' &&
+              menu.timeSelect !== '3時間30分' &&
+              menu.timeSelect !== '4時間' &&
+              menu.timeSelect !== '4時間30分' &&
+              menu.timeSelect !== '5時間'
+            ) {
+              menuErrorMessage.push(
+                `メニュー${index + 1}の処置時間は選択肢から選んでください。`
+              )
+            }
           }
-          // 処置時間に任意の文字が入力されている
-          else if (!timeCheck) {
-            alert("処置時間は選択肢から選んでください。");
-            error = true;
-            break;
-          }
-        }
+        })
+      }
+      // エラー出力
+      if (menuErrorMessage.length) {
+        alert(menuErrorMessage.join('\n'))
+        error = true
       }
       // ---------------------------
       // ---------------------------
-      // エラー処理(profilePicture)---
-      // profilePictureが空の場合
+      // エラー処理(profilePictures)---
+      // profilePicturesが空の場合
       if (this.isEmpty(this.profilePictures)) {
-        alert("プロフィール画像を入力してください。")
-        error = true;
+        profilePictureErrorMessage.push('プロフィール画像を入力してください。')
       }
-      // profilePictureが空でない場合
+      // profilePicturesが空でない場合
       else {
-        for (let i = 0; i < this.profilePictures.length; i++) {
+        this.profilePictures.forEach((profilePicture, index) => {
           // 画像が未入力
-          if (this.profilePictures[i].image === null) {
-            alert("プロフィール画像を入力してください。");
-            error = true;
-            break;
+          if (profilePicture.image === null) {
+            profilePictureErrorMessage.push(
+              `画像${index + 1}のプロフィール画像を入力してください。`
+            )
           }
-        }
+        })
+      }
+      // エラー出力
+      if (profilePictureErrorMessage.length) {
+        alert(profilePictureErrorMessage.join('\n'))
+        error = true
       }
       // ---------------------------
       // エラーがなければ、確認画面へ(引数：profile, menu, profilePicture)
       if (!error) {
         // 確認画面に遷移する
         this.$router.push({
-          path: "/checkBeauticianInformation",
-          query: { profile: this.profile, menus: this.menus, profilePictures: this.profilePictures },
-        });
+          path: '/checkBeauticianInformation',
+          query: {
+            profile: this.profile,
+            menus: this.menus,
+            profilePictures: this.profilePictures,
+          },
+        })
       }
     },
-    isEmpty(obj){
+    isEmpty(obj) {
       // オブジェクトが空かどうか判定
-      return !Object.keys(obj).length;
+      return !Object.keys(obj).length
     },
   },
 }
